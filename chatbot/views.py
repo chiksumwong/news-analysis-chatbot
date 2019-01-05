254db5c4ca7f6bb38080a9bbee9c539ef1a1f4a2
CHIK SUM <chiksum.wong@gmail.com>
2019-01-05 23:08:49 +0800

    Update views.py

--- chatbot/views.py
+++ chatbot/views.py
@@ -2,17 +2,18 @@ from django.conf import settings
 from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseForbidden
 from django.views.decorators.csrf import csrf_exempt
 
-from linebot import LineBotApi, WebhookHandler
+from linebot import LineBotApi, WebhookParser
 from linebot.exceptions import InvalidSignatureError, LineBotApiError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
 
 line_bot_api = LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN)
-handler = WebhookHandler(settings.LINE_CHANNEL_SECRET)
+handler = WebhookParser(settings.LINE_CHANNEL_SECRET)
 
 
 @csrf_exempt
 def callback(request):
+
     if request.method == 'POST':
         signature = request.META['HTTP_X_LINE_SIGNATURE']
         body = request.body.decode('utf-8')
@@ -26,12 +27,10 @@ def callback(request):
 
         for event in events:
             if isinstance(event, MessageEvent):
-                if isinstance(event.message, TextMessage):
-                    line_bot_api.reply_message(
-                        event.reply_token,
-                        TextSendMessage(text=event.message.text)
-                    )
-
+                line_bot_api.reply_message(
+                    event.reply_token,
+                   TextSendMessage(text=event.message.text)
+                )
         return HttpResponse()
     else:
         return HttpResponseBadRequest()
