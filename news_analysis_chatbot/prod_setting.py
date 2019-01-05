8471d3df9d8f4d3d06c6256239702c3375145789
CHIK SUM <chiksum.wong@gmail.com>
2019-01-05 22:47:50 +0800

    production_setting

--- news_analysis_chatbot/prod_setting.py
+++
@@ -1,9 +0,0 @@
-from .setting import *
-import dj_database_url
-DATABASE = {
-    'default' : dj_database_url.config()
-}
-STATIC_ROOT = ‘static’
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-ALLOWED_HOSTS = ['news-analysis-chatbot.herokuapp.com']
-DEBUG = False
