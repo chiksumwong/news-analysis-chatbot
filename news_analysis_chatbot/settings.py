711456e5e223e45c7e024eaf24c0bb0acdb09680
CHIK SUM <chiksum.wong@gmail.com>
2019-01-05 22:51:57 +0800

    Update settings.py

--- news_analysis_chatbot/settings.py
+++ news_analysis_chatbot/settings.py
@@ -39,7 +39,7 @@ SECRET_KEY = '*odlj5j50vvzbfgg%rw!2kk-h82*2!*k@cy9$umzu8ib)w9t+('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['news-analysis-chatbot.herokuapp.com']
 
 
 # Application definition
