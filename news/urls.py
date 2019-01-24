c6c7ebfb162aca2eabdc7abe7936ab76f79c17ac
CHIK SUM <chiksum.wong@gmail.com>
2019-01-24 14:21:19 +0800

    get news info form given url

--- news/urls.py
+++ news/urls.py
@@ -3,6 +3,7 @@ from rest_framework_mongoengine import routers as merouters
 
 from news.views import NewsViewSet
 from news.views import FakeNewsDector
+from news.views import NewsInfo
 
 merouter = merouters.DefaultRouter()
 merouter.register(r'news', NewsViewSet)
@@ -11,6 +12,7 @@ urlpatterns = [
     path('checknews/', FakeNewsDector.check_news),
     path('checknewsbyurl/', FakeNewsDector.check_news_by_url),
     path('info/', FakeNewsDector.get_news_info),
+    path('newsinfo/', NewsInfo.get_news_info_from_url),
 ]
  
 urlpatterns += merouter.urls
